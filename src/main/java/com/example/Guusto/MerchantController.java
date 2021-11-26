b03360b7bc236ed08948cce829930674a6c65a2f
Logan <logan.masniuk@crowdcontent.com>
2021-11-25 20:20:42 -0800

    Retrieve JSON data(array) from file and return data in GET response.

--- src/main/java/com/example/Guusto/MerchantController.java
+++ src/main/java/com/example/Guusto/MerchantController.java
@@ -4,13 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,39 +19,25 @@ public class MerchantController {
 
     @CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/merchants")
-	public Merchant getMerchants(
-        @RequestParam(value = "name", defaultValue = "Example Merchant") String name,
-        @RequestParam(value = "country", defaultValue = "Example Country") String country
-    ) 
+	public List<Merchant> getMerchants() 
     {
         ObjectMapper mapper = new ObjectMapper();
+        List<Merchant> merchants = new ArrayList<Merchant>();
         
         try {
-            //Get JSON array and print each Array element out
             InputStream inputStream = new FileInputStream(new File("./src/main/resources/exampleMerchantsData.json"));
-            Merchant[] merchants = mapper.readValue(inputStream, Merchant[].class);
+            merchants = Arrays.asList(mapper.readValue(inputStream, Merchant[].class));
+
             for(Merchant m: merchants) {
                 System.out.println(m);
             }
-
-            //Return a single merchant
-            String jsonMerchant = getMerchants();
-            Merchant merchant = mapper.readValue(jsonMerchant, Merchant.class);
-            return merchant;
         }
         catch (IOException e)
         {
             System.out.println("Exception Encountered");
+            System.out.println(e);
         }
-        
-        return new Merchant(name, country);
+
+        return merchants;
 	}
-
-    public String getMerchants(){
-        JSONObject merchant = new JSONObject();
-        merchant.put("name", "Cactus Club");
-        merchant.put("country", "Canada");
-
-        return merchant.toString();
-    }	
 }
