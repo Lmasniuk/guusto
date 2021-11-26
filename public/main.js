9a7eaf4f895f47081b0320263b082ca901d3b333
Logan <logan.masniuk@crowdcontent.com>
2021-11-25 22:20:53 -0800

    Allow Filtering of Merchants based on Country Selection

--- public/main.js
+++ public/main.js
@@ -5,3 +5,26 @@ angular.module('demo', [])
             $scope.merchants = response.data;
         });
 });
+
+
+function countryVisibility() {
+    var countryFilter = "";
+    if (document.getElementById('canada').checked){
+        countryFilter = 'Canada';
+    } else if (document.getElementById('usa').checked) {
+        countryFilter = 'USA';
+    } else if (document.getElementById('all-countries').checked) {
+        countryFilter = 'all';
+    }
+
+    $('.merchant-listing').each(function(){
+        var merchantCountry = $(this).find("td:eq(1)").text();
+        if(countryFilter == "all") {
+            $(this).css("display", "block");
+        } else if(merchantCountry == countryFilter){
+            $(this).css("display", "block");
+        } else {
+            $(this).css("display", "none");
+        }
+     });
+}
